ff453094c21ff36ecda9de1e7793c6a386c1a139
Ishaan Sheikh <sheikh.ysbh@gmail.com>
2023-02-08 14:13:30 +0530

    chore: updated filament config

--- config/filament.php
+++ config/filament.php
@@ -61,7 +61,7 @@ return [
     |
     */
 
-    'home_url' => '/',
+    'home_url' => '/app',
 
     /*
     |--------------------------------------------------------------------------
@@ -236,7 +236,7 @@ return [
             'have_inline_labels' => false,
         ],
         'footer' => [
-            'should_show_logo' => true,
+            'should_show_logo' => false,
         ],
         'max_content_width' => null,
         'notifications' => [
