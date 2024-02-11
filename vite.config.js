3af1d103f8527173bbc60c6f70937cf01333337f
Ishaan Sheikh <sheikh.ysbh@gmail.com>
2024-02-11 15:28:08 +0530

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/css/filament.css',
-                'resources/js/app.js'
-            ],
+            input: ["resources/js/app.js"],
             refresh: true,
         }),
     ],
