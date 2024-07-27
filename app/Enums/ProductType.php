9de7fbe42deaf38149cdad1d40117a6b0f3d563e
Ishaan Sheikh <sheikh.ysbh@gmail.com>
2024-07-27 13:23:37 +0530

    fix: product type column label

--- app/Enums/ProductType.php
+++ app/Enums/ProductType.php
@@ -2,8 +2,27 @@
 
 namespace App\Enums;
 
-enum ProductType: int
+use Filament\Support\Contracts\HasColor;
+use Filament\Support\Contracts\HasLabel;
+
+enum ProductType: int implements HasColor, HasLabel
 {
     case Service = 1;
     case Physical = 2;
+
+    public function getLabel(): string
+    {
+        return match ($this) {
+            self::Service => 'Service',
+            self::Physical => 'Physical',
+        };
+    }
+
+    public function getColor(): string | array | null
+    {
+        return match ($this) {
+            self::Service => 'success',
+            self::Physical => 'warning'
+        };
+    }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Enums\ProductType;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -11,5 +12,9 @@ class Product extends Model
 
     protected $fillable = [
         'product_id', 'name', 'type', 'price', 'is_available'
+    ];
+
+    protected $casts = [
+        'type' => ProductType::class
     ];
 }
