8a1d3984cff5d8ca9e343d443ab83bf160b827e5
Ishan Sheikh <sheikh.ysbh@gmail.com>
2025-08-16 16:17:16 +0530

    wip

--- config/app.php
+++ config/app.php
@@ -194,7 +194,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\Filament\AppPanelProvider::class,
-        App\Providers\Filament\AdminPanelProvider::class,
         App\Providers\RouteServiceProvider::class,
 
     ],
