f89f6b14cca1fee9a46d2eb2a241129314d4daa2
Ishan Sheikh <sheikh.ysbh@gmail.com>
2025-09-10 11:08:13 +0530

    fix: allow all users to login to panel

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,6 +50,6 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return $this->email == "admin@tinycrm.com";
+        return true;
     }
 }
